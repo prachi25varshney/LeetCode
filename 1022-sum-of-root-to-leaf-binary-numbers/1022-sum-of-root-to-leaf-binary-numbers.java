622b51df0a65bd30af3e442898444284e3c4cdcd
Prachi Varshney <65917234+prachi25varshney@users.noreply.github.com>
2022-01-11 23:30:13 +0530

    Time: 1 ms (50.87%), Space: 39.6 MB (15.88%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -16,36 +16,25 @@
 
  
 class Solution {
-    int sum =0;
+    int sum = 0;
     public int sumRootToLeaf(TreeNode root) {
-        if(root.left==null && root.right==null)
-            return root.val;
-        sumrootToLeafUtil(root, new ArrayList<>());
-        return sum;   
+        if(root == null) return 0;
+        sumRootToLeafUtil(root, 0);
+        return sum;
     }
-    public void sumrootToLeafUtil(TreeNode root, List<Integer> temp){
-        if(root.left==null && root.right==null){
-            temp.add(root.val);
-            sum+= convertBinaryToDecimal(temp);
-            temp.remove(temp.size()-1);
-            return;
+    public void sumRootToLeafUtil(TreeNode root, int num){
+        if(root == null) return ;
+        num = num*2 + root.val;
+        
+        if(root.left == null && root.right == null){
+            sum += num;
+            return ;
         }
-        temp.add(root.val);  
-        if(root.left!=null){
-            sumrootToLeafUtil(root.left,temp);
+        if(root.left != null){
+            sumRootToLeafUtil(root.left, num);
         }
-        if(root.right!=null){
-            sumrootToLeafUtil(root.right,temp);
+        if(root.right != null){
+            sumRootToLeafUtil(root.right, num);
         }
-        temp.remove(temp.size()-1);
-    }
-    private int convertBinaryToDecimal(List<Integer> arr){
-        int decimal=0,base=1;
-        for(int i= arr.size()-1 ;i>=0;i--){
-            if(arr.get(i)==1)
-                decimal+=base;
-            base = base*2;
-        }
-        return decimal;
     }
 }
