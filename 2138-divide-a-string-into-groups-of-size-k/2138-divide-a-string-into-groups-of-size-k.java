8390215f11935e94569f9630aaa81208ee279a70
Prachi Varshney <65917234+prachi25varshney@users.noreply.github.com>
2022-01-16 13:45:36 +0530

    Time: 1 ms (100.00%), Space: 39.6 MB (50.00%) - LeetHub

--- 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.java
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.java
@@ -1,24 +1,15 @@
 class Solution {
     public String[] divideString(String s, int k, char fill) {
-        int n;
-        if(s.length()%k==0)
-            n=s.length()/k;
-        else
-            n=s.length()/k +1;
+        StringBuilder str = new StringBuilder(s);
+        while(str.length()%k!=0)
+            str.append(fill);
+        s = str.toString();
+        int n = s.length()/k;
         String[] result = new String[n];
         int j=0;
         for(int i=0;i<s.length();i=i+k){
-            if(i+k<=s.length())
-                result[j++] = s.substring(i,i+k);
-            else{
-                StringBuilder str = new StringBuilder();
-                str.append(s.substring(i,s.length()));
-                int length = str.length();
-                for(int z =0;z<k-length;z++){
-                    str.append(fill);
-                }
-                result[j++]  = str.toString();
-            }
+            result[j++] = s.substring(i,i+k);
+           
         }
         return result;
     }
