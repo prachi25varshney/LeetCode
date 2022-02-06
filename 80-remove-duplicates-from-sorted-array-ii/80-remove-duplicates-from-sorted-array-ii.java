bc7ed4f07cbb3c2427eac978c52c09a79a1c2014
Prachi Varshney <65917234+prachi25varshney@users.noreply.github.com>
2022-02-06 23:10:35 +0530

    Time: 5 ms (5.45%), Space: 44.9 MB (6.26%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
@@ -1,17 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         HashMap<Integer,Integer> hashMap = new HashMap<>();
-        ArrayList<Integer> list = new ArrayList<>();
+        int index=0;
         for(int num : nums){
             if(!hashMap.containsKey(num) || hashMap.get(num)<2){
                 hashMap.put(num, hashMap.getOrDefault(num,0)+1);
-                list.add(num);
+                nums[index++] = num;
             }
         }
-        int i=0;
-        for(int num : list){
-            nums[i++] = num;
-        }
-        return list.size();
+        return index;
     }
 }
