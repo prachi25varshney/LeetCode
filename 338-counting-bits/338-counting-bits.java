d265352f2c6e30261c8a771caec386f504e56bb1
Prachi Varshney <65917234+prachi25varshney@users.noreply.github.com>
2022-03-01 22:30:00 +0530

    Time: 3 ms (57.63%), Space: 48.3 MB (24.49%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -2,27 +2,10 @@ class Solution {
     StringBuilder str = new StringBuilder();
     public int[] countBits(int n) {
         int[] ans = new int[n+1];
-        for(int i=1;i<=n;i++){
-            str.setLength(0);
-            binary(i);
-            int ones = countOnes(str.toString());
-            ans[i] = ones;
+        for(int i=0; i<=n; i++)
+        {
+            ans[i] = i%2 == 0 ? ans[i/2] : 1 + ans[(i-1)/2];
         }
         return ans;
-    }
-    public void binary(int n)
-    {
-        if (n > 1)
-            binary(n / 2);
- 
-        str.append(n % 2);
-    }
-    public int countOnes(String bin){
-        int count =0;
-        for(char ch : bin.toCharArray()){
-            if(ch=='1')
-                count++;
-        }
-        return count;
     }
 }
