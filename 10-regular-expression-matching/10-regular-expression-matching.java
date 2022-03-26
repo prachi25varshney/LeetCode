10ad2853ff08beb8c2ba67af6bd5652965f61b34
Prachi Varshney <65917234+prachi25varshney@users.noreply.github.com>
2022-03-26 22:27:59 +0530

    Time: 3 ms (84.06%), Space: 42.5 MB (68.14%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.java
+++ 10-regular-expression-matching/10-regular-expression-matching.java
@@ -8,18 +8,18 @@ class Solution {
                 dp[0][i + 1] = true;
         }
         
-        for(int i = 0; i<s.length(); i++) {
-            for(int j = 0; j<p.length(); j++) {
-                if(p.charAt(j) != '.' && p.charAt(j) != '*') {
-                    dp[i + 1][j + 1] = dp[i][j] && (p.charAt(j) == s.charAt(i));
-                } else if(p.charAt(j) == '.') {
-                    dp[i + 1][j + 1] = dp[i][j];
+        for(int i = 1; i<=s.length(); i++) {
+            for(int j = 1; j<=p.length(); j++) {
+                if(p.charAt(j-1) != '.' && p.charAt(j-1) != '*') {
+                    dp[i][j] = dp[i-1][j-1] && (p.charAt(j-1) == s.charAt(i-1));
+                } else if(p.charAt(j-1) == '.') {
+                    dp[i][j] = dp[i-1][j-1];
                 } else {
-                    dp[i + 1][j + 1] = dp[i + 1][j];
-                    if(j > 0)
-                        dp[i + 1][j + 1] = dp[i + 1][j + 1] 
-                        || dp[i + 1][j - 1]
-                        || (dp[i][j + 1] && (s.charAt(i) == p.charAt(j - 1) || p.charAt(j - 1) == '.'));
+                    dp[i][j] = dp[i][j-1];
+                    if(j-1 > 0)
+                        dp[i][j] = dp[i][j] 
+                        || dp[i][j - 2]
+                        || (dp[i-1][j] && (s.charAt(i-1) == p.charAt(j - 2) || p.charAt(j - 2) == '.'));
                 }
             }
         }
