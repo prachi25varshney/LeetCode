4c21cf98ba8f0744551613539151d659f4c91e94
Prachi Varshney <65917234+prachi25varshney@users.noreply.github.com>
2022-03-27 12:40:36 +0530

    Time: 3 ms (60.90%), Space: 48 MB (75.62%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,29 +1,30 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        List<Integer> list = new ArrayList<>();
-        int[] count= new int[mat.length];
-        int[] arr = new int[mat.length];
-        for(int i =0;i<mat.length;i++){
-            arr[i] = i;
-            for(int j : mat[i]){
-                if(j==1)
-                    count[i]++;
+        int[] nums = new int[mat.length];
+        int[] result = new int[k];
+        
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[i].length;j++){
+                if(mat[i][j] == 1) nums[i]++;
             }
         }
         
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=0;j<arr.length-i-1;j++){
-                if(count[arr[j]]>count[arr[j+1]]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+        int indexer = 0;
+        
+        while (k > 0) {
+            int minIndex = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] < nums[minIndex]) {
+                    minIndex = i;
                 }
             }
+
+            result[indexer++] = minIndex;
+            nums[minIndex] = Integer.MAX_VALUE;
+           
+            k--;
         }
-        int retArr[] =new int[k];
-        for(int i=0;i<k;i++){
-            retArr[i] = arr[i];
-        }
-        return retArr;
+
+        return result;
     }
 }
