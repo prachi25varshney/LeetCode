8616893da7a4e80c0f3791e5fe977f0ee491e7fd
Prachi Varshney <65917234+prachi25varshney@users.noreply.github.com>
2022-04-15 15:03:45 +0530

    Time: 0 ms (100.00%), Space: 45.3 MB (81.89%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
@@ -14,13 +14,11 @@
  * }
  */
 class Solution {
-    int count =0;
+
     public int countNodes(TreeNode root) {
         if(root == null)
             return 0;
-        count++;
-        countNodes(root.left);
-        countNodes(root.right);
-        return count;
+        
+        return 1 + countNodes(root.left) + countNodes(root.right);
     }
 }
