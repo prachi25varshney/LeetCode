e5d9b2e7ac7f666e2a00e012f2dcaedcbe3a07df
Prachi Varshney <65917234+prachi25varshney@users.noreply.github.com>
2022-04-27 00:29:18 +0530

    Time: 32 ms (98.66%), Space: 43.5 MB (97.28%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,31 +1,34 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
-        PriorityQueue<Pair<Integer,Integer>> minHeap = new PriorityQueue<>((a,b)->a.getKey()-b.getKey());
+        int[] costs = new int[n];
+        Arrays.fill(costs,Integer.MAX_VALUE);
+        costs[0] =0;
         
         boolean[] visited = new boolean[n];
-        
-        minHeap.add(new Pair(0,0));
-        
         int minCost =0;
         int edge =0;
         
         while(edge<n){
-            Pair<Integer,Integer> top = minHeap.poll();
-            int weight = top.getKey();
-            int currEdge = top.getValue();
-            
-            if(visited[currEdge])
-                continue;
+            int minWeight = Integer.MAX_VALUE;
+            int currEdge = -1;
+           for(int i=0;i<n;i++) 
+           { 
+               if(!visited[i] && minWeight>costs[i]){
+                    minWeight = costs[i];
+                    currEdge = i;
+            }    
+           }
             visited[currEdge] = true;
-            minCost = minCost + weight;
+            minCost = minCost + minWeight;
             edge++;
             
             for(int nextNode =0; nextNode<n ; nextNode++){
                 if(!visited[nextNode]){
                     int dis = Math.abs(points[currEdge][0]-points[nextNode][0]) +
                         Math.abs(points[currEdge][1]-points[nextNode][1]);
-                    minHeap.add(new Pair(dis,nextNode));
+                    if(costs[nextNode]> dis)
+                        costs[nextNode] = dis;
                 }
             }
         }
