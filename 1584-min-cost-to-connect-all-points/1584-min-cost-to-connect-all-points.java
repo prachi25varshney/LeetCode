49cdf90f6feeb898cdfdec82e3f2b6f298e4cd0f
Prachi Varshney <65917234+prachi25varshney@users.noreply.github.com>
2022-04-30 17:23:25 +0530

    Time: 158 ms (63.71%), Space: 104.3 MB (50.09%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,37 +1,42 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)-> a.key-b.key);
+        pq.add(new Pair(0,0));
         int n = points.length;
-        int[] costs = new int[n];
-        Arrays.fill(costs,Integer.MAX_VALUE);
-        costs[0] =0;
-        
+        int[] cost = new int[n];
         boolean[] visited = new boolean[n];
-        int minCost =0;
-        int edge =0;
-        
+        int totalCost =0, edge=0;
         while(edge<n){
-            int minWeight = Integer.MAX_VALUE;
-            int currEdge = -1;
-           for(int i=0;i<n;i++) 
-           { 
-               if(!visited[i] && minWeight>costs[i]){
-                    minWeight = costs[i];
-                    currEdge = i;
-            }    
-           }
-            visited[currEdge] = true;
-            minCost = minCost + minWeight;
+            Pair top = pq.poll();
+            int currCost = top.getKey();
+            int currPoint = top.getValue();
+            if(visited[currPoint])
+                continue;
+            visited[currPoint] = true;
+            totalCost += currCost;
             edge++;
-            
-            for(int nextNode =0; nextNode<n ; nextNode++){
-                if(!visited[nextNode]){
-                    int dis = Math.abs(points[currEdge][0]-points[nextNode][0]) +
-                        Math.abs(points[currEdge][1]-points[nextNode][1]);
-                    if(costs[nextNode]> dis)
-                        costs[nextNode] = dis;
+            for(int nextPoint=0;nextPoint<n;nextPoint++){
+                if(!visited[nextPoint]){
+                    int dis = Math.abs(points[currPoint][0]-points[nextPoint][0]) +
+                        Math.abs(points[currPoint][1]-points[nextPoint][1]);
+                    pq.add(new Pair(dis, nextPoint));
                 }
             }
+            
         }
-        return minCost;
+        return totalCost;
+    }
+    class Pair{
+        int key; int value;
+        Pair(int key, int value){
+            this.key = key;
+            this.value = value;
+        }
+        int getKey(){
+            return this.key;
+        }
+        int getValue(){
+            return this.value;
+        }
     }
 }
