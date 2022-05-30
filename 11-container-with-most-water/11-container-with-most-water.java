8e0d1f95a9b9a8f5f122339d96e5cf5a362e113c
Prachi Varshney <65917234+prachi25varshney@users.noreply.github.com>
2022-05-30 10:38:12 +0530

    Time: 6 ms (26.77%), Space: 80.9 MB (41.19%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -1,22 +1,37 @@
 class Solution {
     public int maxArea(int[] height) {
-       int l=0;
-        int r=height.length-1;
-        int area =0;
-        while(l<r){
-            int lheight = height[l];
-            int rheight = height[r];
-            area = Math.max(area,((r-l)*Math.min(height[l],height[r])));
-            if(height[l]<height[r]){
-                while(l<r && height[l] <= lheight)
-                    l++;
-            }
-            else{
-                while(l<r && height[r] <= rheight)
-                    r--;
-            }
+//        int l=0;
+//         int r=height.length-1;
+//         int area =0;
+//         while(l<r){
+//             int lheight = height[l];
+//             int rheight = height[r];
+//             area = Math.max(area,((r-l)*Math.min(height[l],height[r])));
+//             if(height[l]<height[r]){
+//                 while(l<r && height[l] <= lheight)
+//                     l++;
+//             }
+//             else{
+//                 while(l<r && height[r] <= rheight)
+//                     r--;
+//             }
                 
+//         }
+//         return area;
+        int l = 0;
+        int r = height.length - 1;
+        int result = Integer.MIN_VALUE;
+        int currentArea = Integer.MIN_VALUE;
+        while ( l < r) {
+            currentArea = Math.min( height[l], height[r] ) * (r -l);
+            result = Math.max( currentArea, result );
+            
+            if ( height[l] < height[r] ){
+                l++;
+            } else {
+                r--;
+            }
         }
-        return area;
+        return result;
     }
 }
